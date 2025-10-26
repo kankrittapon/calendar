21dc4959eaef296ee22bccd9b61049053232ff6f
kankrittapon <123614247+kankrittapon@users.noreply.github.com>
2025-10-26 15:29:28 +0700

    แก้ไขการเพิ่มงาน

--- src/indexsecretary.js
+++ src/indexsecretary.js
@@ -52,9 +52,9 @@ td{padding:12px;border-bottom:1px solid var(--border)}
         <option value="16:00">16:00</option><option value="16:30">16:30</option><option value="17:00">17:00</option>
       </select>
     </label></div>
-    <div><label>เวลาจบ
+    <div><label>เวลาจบ (ไม่จำเป็น)
       <select id="end">
-        <option value="">เลือกเวลา</option>
+        <option value="">ไม่ระบุ</option>
         <option value="09:00">09:00</option><option value="09:30">09:30</option><option value="10:00">10:00</option>
         <option value="10:30">10:30</option><option value="11:00">11:00</option><option value="11:30">11:30</option>
         <option value="12:00">12:00</option><option value="12:30">12:30</option><option value="13:00">13:00</option>
